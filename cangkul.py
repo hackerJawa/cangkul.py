13cfd51bc346896afbe9eefd06ee95d4a333c7e3
hackerJawa <74855045+hackerJawa@users.noreply.github.com>
2020-11-22 16:01:41 +0700

    Create cangkul.py

---
+++ cangkul.py
@@ -0,0 +1,20 @@
+import os
+
+print("code by : hacker_jawa")
+print("TOOLS CANGKUL DDOS! ")
+print("ADMIN TIDAK TANGGUNG JAWAB BILA ADA KERUSAKAN")
+print("GUNAKAN DENGAN BIJAK")
+print("nama website harus memakai https, contoh https://tiktok.com")
+print("\n")
+
+
+nomor = input("masukan nama website: ")
+kirim = 2000
+
+os.system.('clear')
+
+for lol in range ((int(kirim))):
+  if lol > 500:
+    print("server tidak di kenali! Sepertinya Down!")
+  else: 
+    print("sedang mengirim DDOS!")
